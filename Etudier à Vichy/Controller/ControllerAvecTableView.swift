b30aba1c87c8a5f672273cf552d79900d3ced3e7
David928 <david.gonzales928@gmail.com>
2018-08-24 21:04:16 +0200

    Ajout des nouveaux contrôleurs

---
+++ Etudier à Vichy/Controller/ControllerAvecTableView.swift
@@ -0,0 +1,30 @@
+//
+//  ControllerAvecTableView.swift
+//  Etudier à Vichy
+//
+//  Created by GONZALES David on 24/08/2018.
+//  Copyright © 2018 GONZALES David. All rights reserved.
+//
+
+import UIKit
+
+class ControllerAvecTableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
